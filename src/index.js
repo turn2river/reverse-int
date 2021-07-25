d104c2aa35aaab3d0642416ec34628af47f35785
alexey.krylov <turn2river@gmail.com>
2021-07-25 22:52:49 +0300

    feat: add solution to reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    n = -n;
+    return n = +n.toString().split("").reverse().join("");    
+  }
+  return n = +n.toString().split("").reverse().join("");      
 }
