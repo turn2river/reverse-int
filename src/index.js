d1dc4d97a8ced341a148a6254e36bf235c7ff444
alexey.krylov <turn2river@gmail.com>
2022-01-06 19:13:07 +0300

    refactor: change the solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 module.exports = function reverse (n) {
-  if (n < 0) {
-    n = -n;
-    return n = +n.toString().split("").reverse().join("");    
-  }
-  return n = +n.toString().split("").reverse().join("");      
+  Math.abs(n);
+  return +n.toString().split('').reverse().join('');       
 }
+
+
